f473ee0e96806d8b5266e157d5b0350345d38c70
Prakash Rajak <kucchiprakash@gmail.com>
2024-01-08 23:26:51 +0530

    file change

---
+++ app.js
@@ -0,0 +1,8 @@
+let head =React.createElement(
+    "h1",
+ {id:"mona"},
+  "jai shree ram sitaram");
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(head);
+console.log(head)
+console.log(root);
